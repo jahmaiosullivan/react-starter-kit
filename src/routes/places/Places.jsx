19a195fce959cae5c79d3e071af7035ac720ec68
Jahmai OSullivan <jaosulli@microsoft.com>
2016-07-14 20:41:29 -0400

    Add react bootstrap grid

--- src/routes/places/Places.jsx
+++ src/routes/places/Places.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Places.css';
-
+import {Grid, Row, Col, Clearfix} from 'react-bootstrap';
 const title = 'My places';
 
 
@@ -10,7 +10,16 @@ function Places(props, context) {
     <div className={styles.root}>
       <div className={styles.container}>
         <h1>{title}</h1>
-
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12} md={8}>Left Side</Col>
+            <Col xs={6} md={4}>Right side</Col>
+          </Row>
+          <Row className="show-grid">
+            <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+            <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+          </Row>
+        </Grid>
       </div>
     </div>
   );
