c89c52ede6b40aa6f53541f2a6746f2b007d0864
Jahmai OSullivan <jaosulli@microsoft.com>
2016-07-16 01:53:34 -0400

    Add button to create a group

--- src/routes/groups/Groups.js
+++ src/routes/groups/Groups.js
@@ -13,12 +13,12 @@ import cover7 from './cover7.jpg';
 import cover8 from './cover8.jpg';
 import cover9 from './cover9.jpg';
 var cx = classNames.bind(styles);
-import {Grid, Row, Col, Clearfix} from 'react-bootstrap';
+import {Grid, Row, Col, ButtonToolbar, Button, Clearfix} from 'react-bootstrap';
 
 
 const title = 'Groups';
 
-function Group({img, name }) {
+function Group({img, name}) {
   return (
     <Col xs={12} sm={6} md={4}>
       <div className={styles.group}>
@@ -39,18 +39,18 @@ function Groups(props, context) {
   return (
     <div className={styles.root}>
       <div className={styles.container}>
-        <h1>{title}</h1>
+        <h1>{title} <Button bsStyle="primary">Start group</Button></h1> 
         <Grid fluid={true}>
           <Row className={styles.noGutter}>
-            <Group img={cover1} name="Group 1" />
-            <Group img={cover2} name="Group 2" />
-            <Group img={cover3} name="Group 3" />
-            <Group img={cover4} name="Group 4" />
-            <Group img={cover5} name="Group 5" />
-            <Group img={cover6} name="Group 6" />
-            <Group img={cover7} name="Group 7" />
-            <Group img={cover8} name="Group 8" />
-            <Group img={cover9} name="Group 9" />
+            <Group img={cover1} name="Group 1"/>
+            <Group img={cover2} name="Group 2"/>
+            <Group img={cover3} name="Group 3"/>
+            <Group img={cover4} name="Group 4"/>
+            <Group img={cover5} name="Group 5"/>
+            <Group img={cover6} name="Group 6"/>
+            <Group img={cover7} name="Group 7"/>
+            <Group img={cover8} name="Group 8"/>
+            <Group img={cover9} name="Group 9"/>
           </Row>
         </Grid>
       </div>
