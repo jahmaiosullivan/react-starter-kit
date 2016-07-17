c8201f7fd3d481c2ca1eef8bca531181355168ac
Jahmai OSullivan <jaosulli@microsoft.com>
2016-07-17 16:20:44 -0400

    change header text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
@@ -14,18 +5,22 @@ import Link from '../Link';
 import Navigation from '../Navigation';
 import logoUrl from './logo-small.png';
 
+const appName = 'HobbyClue';
+const subText = 'Rabble rabble rabble';
+const city = "Boston, MA";
+
 function Header() {
   return (
     <div className={s.root}>
       <div className={s.container}>
         <Navigation className={s.nav} />
         <Link className={s.brand} to="/">
-          <img src={logoUrl} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
+          <img src={logoUrl} width="38" height="38" alt={appName} />
+          <span className={s.brandTxt}>{appName}</span>
         </Link>
         <div className={s.banner}>
-          <h1 className={s.bannerTitle}>React</h1>
-          <p className={s.bannerDesc}>Complex web apps made easy</p>
+          <h1 className={s.bannerTitle}>{city}</h1>
+          <p className={s.bannerDesc}>{subText}</p>
         </div>
       </div>
     </div>
